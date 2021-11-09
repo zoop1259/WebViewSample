d5faa7b5d8f0e95448dc2a2a7bfe7b3017cb6d54
zoop1259 <kdm432191@gmail.com>
2021-11-09 15:52:28 +0900

    웹뷰

--- WebViewSample/ViewController.swift
+++ WebViewSample/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var WebViewMain: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //1. url string을 준비.
+        //2. url 주소를 request로 만들기
+        //3. req 를 load
+        
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) {
+           //언래핑(옵셔널 바인딩) 사용해야한다.
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+        
+        
+        
+        
     }
 
 
